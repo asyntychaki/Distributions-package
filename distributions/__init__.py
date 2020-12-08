920ac5a9759d7af33e39c1361c2c7f28252333f9
Anastasia Syntychaki <asyntychaki@users.noreply.github.com>
2020-12-08 17:11:45 +0200

    Created package initialization file

---
+++ distributions/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 17:06:30 2020
+
+@author: asyntychaki
+"""
+
+from Gaussiandistribution import Gaussian # Initializes the Gaussian class so it can be directly used. The '.' id required only for Python 3. 
