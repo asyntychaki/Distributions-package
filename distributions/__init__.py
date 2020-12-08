38fbc5b50b5489b70902272be7f57a853b927c67
Anastasia Syntychaki <asyntychaki@users.noreply.github.com>
2020-12-08 17:50:05 +0200

    Fixed typo

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -6,4 +6,5 @@ Created on Tue Dec  8 17:06:30 2020
 @author: asyntychaki
 """
 
-from .Gaussiandistribution import Gaussian # Initializes the Gaussian class so it can be directly used. The '.' is required only for Python 3. 
+from .Gaussiandistribution import Gaussian # The '.' is required only for Python 3. 
+# Initializes the Gaussian class so it can be directly used from the distributions package. 
